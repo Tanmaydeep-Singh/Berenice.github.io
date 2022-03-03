94ed5af37aa67e0eb6695012bc73d2a2540113e4
Tanmaydeep Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-03-03 19:32:40 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,13 @@
+var loader = document.getElementById("PreLoader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+
+
+{
+function Hide(){
+  document.getElementById("check").checked = false;
+}
+};
+
